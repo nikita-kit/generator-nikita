4cc8183f791b8b28d44eec2026cea25deb999a3a
Benny Görlitz <benny.goerlitz@exozet.com>
2017-03-13 22:16:38 +0100

    jsbs now need a suffix for autoloading

--- app/index.js
+++ app/index.js
@@ -442,7 +442,7 @@ var NikitaGenerator = yeoman.generators.Base.extend({
 			// JS Files
 			this.template('source/js/_main.js.ejs', sourceFolder + '/js/_main.js');
 			this.template('source/js/app.js.ejs', sourceFolder + '/js/app.js');
-			this.template('source/js/behaviours/Test.js.ejs', sourceFolder + '/js/behaviours/Test.js');
+			this.template('source/js/Test.jsb.js.ejs', sourceFolder + '/js/Test.jsb.js');
 			this.template('source/js/modernizr/cssCheckedTest.js.ejs', sourceFolder + '/js/modernizr/cssCheckedTest.js');
 			this.template('source/js/modernizr/positionStickyTest.js.ejs', sourceFolder + '/js/modernizr/positionStickyTest.js');
 
