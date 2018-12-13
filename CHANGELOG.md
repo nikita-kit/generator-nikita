f2ba707661b506f2a352fc6fbd2f0836d21d2c0a
axten <benny.goerlitz@exozet.com>
2018-12-13 21:32:59 +0100

    added grunt-contrib-htmlmin to minify html

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@
 * updated all dependencies to newest version and improved configs   
 * improved jest code coverage output
 * fixed browserSync page reload when js files are changing
+* added `grunt-contrib-htmlmin` to minify html
 * added `grunt-real-favicon` for app-icon generation
   * place desired image at `static/img/appicons/_source.png`
   * start favicon generator with `grunt generate-appicons`
--- app/index.js
+++ app/index.js
@@ -331,6 +331,7 @@ module.exports = class extends Generator {
         this._copyTemplate('grunt/config/clean.js.ejs', 'grunt/config/clean.js');
         this._copyTemplate('grunt/config/concurrent.js.ejs', 'grunt/config/concurrent.js');
         this._copyTemplate('grunt/config/eslint.js.ejs', 'grunt/config/eslint.js');
+        this._copyTemplate('grunt/config/htmlmin.js.ejs', 'grunt/config/htmlmin.js');
         this._copyTemplate('grunt/config/imagemin.js.ejs', 'grunt/config/imagemin.js');
         this._copyTemplate('grunt/config/jest.js.ejs', 'grunt/config/jest.js');
         this._copyTemplate('grunt/config/postcss.js.ejs', 'grunt/config/postcss.js');
